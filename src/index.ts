054d66633d5dc14d72f4d78e3243dc8083863b0e
adrien zakian <azakian@luccasoftware.com>
2026-03-10 21:19:37 +0100

    chore: automate release workflow with tag-based CI and dynamic version

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,11 @@ import {CreateBranchCommand} from "./command/commands/create-branch";
 import {DeleteBranchesCommand} from "./command/commands/delete-branch";
 import {RenameBranchCommand} from "./command/commands/rename-branch";
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const {version} = require("../package.json");
+
 const cli = new Command()
-cli.version('1.0.0');
+cli.version(version);
 
 cli.addCommand(SwitchBranchCommand)
 cli.addCommand(CreateBranchCommand)
